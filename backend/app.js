7cbe6adf3bbd4d2db60eabb8422366cc92bb3819
max <maxetness@gmail.com>
2022-10-14 12:25:55 +0200

    ajout final

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const userRoutes = require("./routes/user");
+const saucesRoutes = require("./routes/sauces");
+
+mongoose
+  .connect(
+    "mongodb+srv://User:l9f4LiqFp7Utr9d8@cluster0.pa6cysc.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", saucesRoutes);
+const path = require("path");
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+module.exports = app;
---
+++ backend/routes/sauces.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const auth = require("../middleware/auth");
+const router = express.Router();
+const multer = require("../middleware/multer-config");
+
+const sauceCtrl = require("../controllers/sauces");
+
+router.get("/", auth, sauceCtrl.getAllSauces);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.post("/:id/like", auth, multer, sauceCtrl.likeDislike);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+
+module.exports = router;
